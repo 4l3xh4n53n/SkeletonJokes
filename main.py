349c23dceef35fe89278aed131f4890e01f3a195
4l3xh4n53n <omgalex2006@gmail.com>
2025-01-08 18:30:12 +0000

    Loads JSON file into JSON object and reuse get_joke() in return_page()

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, render_template
 from waitress import serve
-
 import json
 import random
 
@@ -8,26 +7,22 @@ app = Flask(__name__)
 jokes = []
 
 
-def load():
+def load_jokes():
     with open('jokes/jokes.json', 'r') as file:
-        data = json.load(file)
-
-    for joke in data['jokes']:
-        jokes.append(joke)
-
-    return jokes
+        d = json.load(file)
+    return d
 
 
 @app.route('/api', methods=['GET'])
 def get_joke():
-    return jsonify(random.choice(jokes))
+    return random.choice(data['jokes'])
 
 
 @app.route('/')
 def return_page():
-    joke_answer = random.choice(jokes)
+    joke_answer = get_joke()
     return render_template('index.html', joke=joke_answer['joke'], answer=joke_answer['answer'])
 
 
-load()
+data = load_jokes()
 serve(app, host="0.0.0.0", port=8080)
